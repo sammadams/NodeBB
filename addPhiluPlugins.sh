5a9a14f4577f10090cfb21f95ce7ab62f96ac9fd
RanaWaqar7550 <rana.waqar@arbisoft.com>
2019-01-07 09:20:31 +0000

    remove add plugins file

--- addPhiluPlugins.sh
+++
@@ -1,6 +0,0 @@
-npm link nodebb-theme-philu-community
-npm link nodebb-plugin-session-sharing
-npm link nodebb-plugin-s3-uploads
-./nodebb activate nodebb-theme-philu-community
-./nodebb activate nodebb-plugin-session-sharing
-./nodebb activate nodebb-plugin-s3-uploads
